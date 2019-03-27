58942a1e6c8cf0fc9ef7f2c29fee9f876c4c07c7
dizzymcfire <48123197+dizzymcfire@users.noreply.github.com>
2019-03-27 16:48:55 +0100

    Add files via upload

---
+++ Project/SCRIPT/mobilscript.js
@@ -0,0 +1,30 @@
+var vidSrc = document.getElementById("myVid");
+
+var isMobile = {
+    Android: function() {
+        return navigator.userAgent.match(/Android/i);
+    },
+    BlackBerry: function() {
+        return navigator.userAgent.match(/BlackBerry/i);
+    },
+    iOS: function() {
+        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+    },
+    Opera: function() {
+        return navigator.userAgent.match(/Opera Mini/i);
+    },
+    Windows: function() {
+        return navigator.userAgent.match(/IEMobile/i);
+    },
+    any: function() {
+        return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+    }
+};
+
+if(isMobile.any()) {
+   console.log("Dette er en mobil så videoen vises ikke");
+}
+
+else {
+  vidSrc.src = "VIDEO/Background_loop2.mp4";
+}
